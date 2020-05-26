84e99336c14104e5361142304cffca01e8788223
innape <innapetrova87@hotmail.com>
2020-05-26 13:09:01 +0100

    Changing image description

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,7 +5,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit}) => {
 	return (
 		<div>
 			<p className ='f3'>
-			{'This Magic Brain will detect faces in your pucture. Try uploading an image.'}
+			{'This Magic Brain will detect faces in your pucture. Specify an image URL.'}
 			</p>
 			<div className='center'>
 				<div className=' form center pa4 br3 shadow-5'>
